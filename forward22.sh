ea4af1bf16f74ce064d491d4ffd93069ae199d5a
Adrian Buford <adrianbuford@gmail.com>
2016-07-25 10:58:12 -0500

    Update forward22.sh

--- forward22.sh
+++ forward22.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ##forward port 22 to port 2222 -- used mostly for kippo / cowrie when not wanting to edit root perms
 
 iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
